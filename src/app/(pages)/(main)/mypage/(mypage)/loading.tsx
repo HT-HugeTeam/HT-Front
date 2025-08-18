995d074524159b014e75627249d37d53a91f6a00
yyj0917 <yyj0917@yonsei.ac.kr>
2025-08-19 02:20:52 +0900

    refactor : global loading.tsx 삭제 -> 개별 페이지 loading.tsx 추

--- src/app/(pages)/(main)/mypage/(mypage)/loading.tsx
+++ src/app/(pages)/(main)/mypage/(mypage)/loading.tsx
@@ -1,6 +1,6 @@
 import { LoadingSpinner } from '@/components/loading-spinner';
 
-export default function RootLoading() {
+export default function MyPageLoading() {
   return (
     <div className='w-full h-full flex-center bg-gray100 bg-blur-lg'>
       <LoadingSpinner />
